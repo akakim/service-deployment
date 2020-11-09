24bea0ff63e86c9190ef7955b5a92bbe1be685d4
akakim <daphne5009@gmail.com>
2020-11-09 13:19:45 +0900

    직관성 향상.
    iaas에서 실례를 추가하여 직관성향상.

--- mysql/deploy.sh
+++ mysql/deploy.sh
@@ -3,7 +3,7 @@
 # VARIABLES
 BOSH_NAME="<BOSH_NAME>"                           # bosh name (e.g. micro-bosh)
 IAAS="<IAAS_NAME>"                                # IaaS (e.g. aws/azure/gcp/openstack/vsphere)
-COMMON_VARS_PATH="<COMMON_VARS_FILE_PATH>"        # common_vars.yml File Path (e.g. /home/ubuntu/paasta-5.0/common/common_vars.yml)
+COMMON_VARS_PATH="<COMMON_VARS_FILE_PATH>"        # common_vars.yml File Path (e.g. /home/ubuntu/paasta-5.0/common/common_vars.yml or /home/ubuntu/paasta-5.0/workspace/paasta-5.0/deployment/cloud-config/{iaas}-cloud-config.yml)
 
 # DEPLOY
 bosh -e ${BOSH_NAME} -n -d mysql deploy --no-redact mysql.yml \
